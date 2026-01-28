c3bbf8a9bd758d8531dfe373d41f8b1bcad41ae9
Collin Wittenstein <cwittens@students.uni-mainz.de>
2026-01-27 19:36:43 -0500

    added changelog

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,30 @@
 using GeothermalWells
 using Documenter
+# Optional: generate a nice changelog page from NEWS.md
+# Requires the `Changelog` package in the `docs` project environment
+try
+    using Changelog
+catch
+    @warn "Changelog.jl not available in docs environment; skip changelog generation"
+end
 
 DocMeta.setdocmeta!(GeothermalWells, :DocTestSetup, :(using GeothermalWells); recursive=true)
+
+# Create changelog
+Changelog.generate(Changelog.Documenter(),
+                    joinpath(@__DIR__, "..", "NEWS.md"),
+                    joinpath(@__DIR__, "src", "changelog.md");
+                    repo = "cwittens/GeothermalWells.jl",
+                    branch = "main")
+# Fix edit URL of changelog
+open(joinpath(@__DIR__, "src", "changelog.md"), "w") do io
+    for line in eachline(joinpath(@__DIR__, "src", "changelog_tmp.md"))
+        if startswith(line, "EditURL")
+            line = "EditURL = \"https://github.com/cwittens/GeothermalWells.jl/blob/main/NEWS.md\""
+        end
+        println(io, line)
+    end
+end
 
 makedocs(;
     modules=[GeothermalWells],
@@ -17,6 +40,7 @@ makedocs(;
         "Home" => "index.md",
         "Methodology" => "methodology.md",
         "Getting Started" => "getting_started.md",
+        "Changelog" => "changelog.md",
         "API Reference" => "reference.md",
         "License" => "license.md",
     ],
